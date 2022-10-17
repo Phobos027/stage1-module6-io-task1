072bd7551351c02a11cd89365b0d25a313da26b4
Phobos027 <shkuro.stepan@gmail.com>
2022-10-17 21:03:30 +0300

    test

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
+    public static void main(String[] args) throws IOException {
+        Profile profile = getDataFromFile(new File("src/main/resources/Profile.txt"));
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        System.out.println(profile);
+    }
+
+    public static Profile getDataFromFile(File file) throws IOException {
+        Profile profile = new Profile();
+        FileInputStream fileInputStream = new FileInputStream(file);
+        String text = "";
+        int ch = 0;
+
+        while ((ch = fileInputStream.read()) != -1) {
+            text += ((char) ch);
+        }
+        fileInputStream.close();
+
+        String[] splitText = text.split(System.lineSeparator());
+
+        String[][] arrayText = new String[splitText.length][];
+        for (int i = 0; i < splitText.length; i++) {
+            arrayText[i] = splitText[i].split(" ");
+        }
+
+        profile.setName(arrayText[0][1]);
+        int age = Integer.parseInt(arrayText[1][1]);
+        long phone = Long.parseLong(arrayText[3][1]);
+        profile.setAge(age);
+        profile.setEmail(arrayText[2][1]);
+        profile.setPhone(phone);
+        return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
     }
 }
